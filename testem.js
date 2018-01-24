57db62334a5890999eb7dbc82e29dd28afe7d445
Randall Morey <randallmorey@randallmorey.com>
2018-01-24 12:57:52 -0500

    add firefox to browser tests

--- testem.js
+++ testem.js
@@ -2,10 +2,12 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'Chrome'
+    'Chrome',
+    'Firefox'
   ],
   launch_in_dev: [
-    'Chrome'
+    'Chrome',
+    'Firefox'
   ],
   browser_args: {
     Chrome: {
@@ -16,6 +18,10 @@ module.exports = {
         '--remote-debugging-port=0',
         '--window-size=1440,900'
       ]
-    }
+    },
+    Firefox: [
+      '-headless',
+      '--window-size=1440,900'
+    ]
   }
 };
